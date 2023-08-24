506c6ab6b8b50e4f2506352ad8dfa923afbc9a42
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-08-24 11:56:24 +0300

    setup backend and use axios to fetch data on frontend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is ready and API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+    const product = products.find((x) => x._id === req.params.id);
+    if(product){
+        res.send(product);
+    } else {
+        res.status(404).send({message: 'Product Not Found'});
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
--- frontend/src/pages/ProductScreen.jsx
+++ frontend/src/pages/ProductScreen.jsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import products from '../products';
 import Rating from "../components/Rating";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
+
   const { id:productId } = useParams();
-  const product = products.find(product => product._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/product/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  },[productId]);
 
   return (
     <>
