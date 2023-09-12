6d1f5d2447b2cb6d5e4147d457ee84bf9869fa5d
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-09-12 17:11:55 +0300

    added backend and front end upload image functionality

--- frontend/src/pages/admin/ProductEditScreen.jsx
+++ frontend/src/pages/admin/ProductEditScreen.jsx
@@ -5,7 +5,11 @@ import Loader from '../../components/Loader';
 import Message from '../../components/Message';
 import FormContainer from '../../components/FormContainer';
 import { toast } from 'react-toastify';
-import { useUpdateProductMutation, useGetProductDetailsQuery } from '../../slices/productsApiSlice';
+import { 
+    useUpdateProductMutation, 
+    useGetProductDetailsQuery,
+    useUploadProductImageMutation,
+} from '../../slices/productsApiSlice';
 
 const ProductEditScreen = () => {
     const { id: productId } = useParams();
@@ -23,6 +27,8 @@ const ProductEditScreen = () => {
     const { data: product, isLoading, error, refetch } = useGetProductDetailsQuery(productId);
 
     const [updateProduct, { isLoading: isLoadingUpdate }] = useUpdateProductMutation();
+
+    const [uploadProductImage, { isLoading: loadingUpload }] = useUploadProductImageMutation();
 
     useEffect(() => {
         if (product) {
@@ -59,6 +65,18 @@ const ProductEditScreen = () => {
         }
     };
 
+    const uploadFileHandler = async (e) => {
+        const formData = new FormData();
+        formData.append('image', e.target.files[0]);
+        try {
+            const res = await uploadProductImage(formData).unwrap();
+            toast.success(res.message);
+            setImage(res.image);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
+
 
   return (
     <>
@@ -91,7 +109,22 @@ const ProductEditScreen = () => {
                         ></Form.Control>
                     </Form.Group>
 
-                    {/*image place holder */}
+                    <Form.Group controlId='image' className='my-2'>
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder='Enter image url'
+                            value={image}
+                            onChange={(e) => setImage}
+                        ></Form.Control>
+                        <Form.Control
+                            type='file'
+                            label='Choose File'
+                            onChange={ uploadFileHandler }
+                        ></Form.Control>
+                    </Form.Group>
+
+
 
                     <Form.Group controlId='brand' className='my-2'>
                         <Form.Label>Brand</Form.Label>
