a74ceacc9854daab5a954b9cbd92f63001b7feb8
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-09-12 14:31:13 +0300

    backend: added update product funtionality and route frontend: added product edit screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,6 +28,7 @@ import OrderScreen from './pages/OrderScreen';
 import ProfileScreen from './pages/ProfileScreen';
 import OrderListScreen from './pages/admin/OrderListScreen';
 import ProductListScreen from './pages/admin/ProductListScreen';
+import ProductEditScreen from './pages/admin/ProductEditScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -49,6 +50,7 @@ const router = createBrowserRouter(
       <Route path='' element={<AdminRoute />} >
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
+        <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
       </Route>
 
     </Route>
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -5,6 +5,6 @@ const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
     baseQuery: baseQuery,
-    tagTypes: ['Products', 'Users', 'Orders'],
+    tagTypes: ['Product', 'Users', 'Orders'],
     endpoints: (builder) => ({}),
 });
