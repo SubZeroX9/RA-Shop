6daa1d5c51220086dfd50e3a89746bf9e1701207
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-09-13 09:34:59 +0300

    added pagination for products

--- frontend/src/pages/admin/ProductListScreen.jsx
+++ frontend/src/pages/admin/ProductListScreen.jsx
@@ -9,9 +9,14 @@ import {
 	useCreateProductMutation,
 	useDeleteProductMutation,
 } from "../../slices/productsApiSlice";
+import { useParams } from "react-router-dom";
+import Paginate from "../../components/Paginate";
 
 const ProductListScreen = () => {
-	const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+	const { pageNumber } = useParams();
+	const { data, isLoading, error, refetch } = useGetProductsQuery({
+		pageNumber,
+	});
 
 	const [createProduct, { isLoading: loadingCreate }] =
 		useCreateProductMutation();
@@ -50,10 +55,7 @@ const ProductListScreen = () => {
 					<h1>Products</h1>
 				</Col>
 				<Col className='text-end'>
-					<Button
-						className='btn-sm my-3'
-						onClick={createProductHandler}
-					>
+					<Button className='btn-sm my-3' onClick={createProductHandler}>
 						<FaPlus /> Create Product
 					</Button>
 				</Col>
@@ -67,52 +69,46 @@ const ProductListScreen = () => {
 			) : error ? (
 				<Message variant='danger'>{error}</Message>
 			) : (
-				<Table
-					striped
-					bordered
-					hover
-					responsive
-					className='table-sm'
-				>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>NAME</th>
-							<th>PRICE</th>
-							<th>CATEGORY</th>
-							<th>BRAND</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{products.map((product) => (
-							<tr key={product._id}>
-								<td>{product._id}</td>
-								<td>{product.name}</td>
-								<td>${product.price}</td>
-								<td>{product.category}</td>
-								<td>{product.brand}</td>
-								<td>
-									<LinkContainer to={`/admin/product/${product._id}/edit`}>
-										<Button
-											variant='light'
-											className='btn-sm mx-2'
-										>
-											<FaEdit />
-										</Button>
-									</LinkContainer>
-									<Button
-										variant='danger'
-										className='btn-sm'
-										onClick={() => deleteHandler(product._id)}
-									>
-										<FaTrash style={{ color: "white" }} />
-									</Button>
-								</td>
+				<>
+					<Table striped bordered hover responsive className='table-sm'>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>NAME</th>
+								<th>PRICE</th>
+								<th>CATEGORY</th>
+								<th>BRAND</th>
+								<th></th>
 							</tr>
-						))}
-					</tbody>
-				</Table>
+						</thead>
+						<tbody>
+							{data.products.map((product) => (
+								<tr key={product._id}>
+									<td>{product._id}</td>
+									<td>{product.name}</td>
+									<td>${product.price}</td>
+									<td>{product.category}</td>
+									<td>{product.brand}</td>
+									<td>
+										<LinkContainer to={`/admin/product/${product._id}/edit`}>
+											<Button variant='light' className='btn-sm mx-2'>
+												<FaEdit />
+											</Button>
+										</LinkContainer>
+										<Button
+											variant='danger'
+											className='btn-sm'
+											onClick={() => deleteHandler(product._id)}
+										>
+											<FaTrash style={{ color: "white" }} />
+										</Button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+					<Paginate pages={data.pages} page={data.page} isAdmin={true} />
+				</>
 			)}
 		</>
 	);
