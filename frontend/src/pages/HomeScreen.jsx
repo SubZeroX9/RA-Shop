c8b4af9d0b05cd74e2dc9aedc477c4fe1e10ae07
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-09-13 10:02:53 +0300

    added search box and search functionality

--- frontend/src/pages/HomeScreen.jsx
+++ frontend/src/pages/HomeScreen.jsx
@@ -4,14 +4,22 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const HomeScreen = () => {
-	const { pageNumber } = useParams();
-	const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+	const { pageNumber, keyword } = useParams();
+	const { data, isLoading, error } = useGetProductsQuery({
+		keyword,
+		pageNumber,
+	});
 
 	return (
 		<>
+			{keyword && (
+				<Link to='/' className='btn btn-light mb-4'>
+					Go Back
+				</Link>
+			)}
 			{isLoading ? (
 				<Loader />
 			) : error ? (
@@ -28,7 +36,11 @@ const HomeScreen = () => {
 							</Col>
 						))}
 					</Row>
-					<Paginate pages={data.pages} page={data.page} />
+					<Paginate
+						pages={data.pages}
+						page={data.page}
+						keyword={keyword ? keyword : ""}
+					/>
 				</>
 			)}
 		</>
